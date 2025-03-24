1deabef9143c84feaa25321eefa65ed135af4950
ignaciozanotti <ignacio.zanotti@gmail.com>
2025-03-24 13:43:51 -0300

    Configure Vite for Vue
    Use Vite as its asset bundler, replacing Mix.

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+
+createInertiaApp({
+  resolve: (name) => import(`./Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(pinia)
+      .mount(el);
+  },
+});
