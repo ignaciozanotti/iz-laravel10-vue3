8a0de7b44028db56c83e5be9dea8d97063935ef9
ignaciozanotti <ignacio.zanotti@gmail.com>
2025-03-24 14:30:28 -0300

    Install Laravel Breeze with Inertia/Vue for authentication
    Install Laravel Breeze for simple auth scaffolding
    
    Run Breeze Installer
    
    Install Node dependencies
    
    Run added tests
    
    Check that Laravel's splashscreen / Welcome.vue works as expected

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,15 +1,23 @@
+import './bootstrap';
+import '../css/app.css';
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
-import { createPinia } from 'pinia';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const pinia = createPinia();
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  resolve: (name) => import(`./Pages/${name}.vue`),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(pinia)
-      .mount(el);
-  },
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
